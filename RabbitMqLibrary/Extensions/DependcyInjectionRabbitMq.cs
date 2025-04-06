7b733c0de10f6ce6aef02b67d31e05f73187ff3d
Gabriel Natan <gabrielnatan.2001.pereira@gmail.com>
2025-04-05 21:58:23 -0300

    fix: atualizando lib para 1.2

--- RabbitMqLibrary/Extensions/DependcyInjectionRabbitMq.cs
+++ RabbitMqLibrary/Extensions/DependcyInjectionRabbitMq.cs
@@ -6,10 +6,10 @@ namespace RabbitMqLibrary.Extensions
 {
     public static class DependcyInjectionRabbitMq
     {
-        public static IServiceCollection AddRabbitMQ(this IServiceCollection services)
+        public static IServiceCollection AddRabbitMQ(this IServiceCollection services, string hostname, string user, string password, int port = 5672)
         {
-            services.AddScoped<IRabbitMqPublisher, RabbitMqPublisher>();
-            services.AddScoped<IRabbitMqConsumer, RabbitMqConsumer>();
+            services.AddScoped<IRabbitMqPublisher>(x => new RabbitMqPublisher(hostname, user, password, port));
+            services.AddScoped<IRabbitMqConsumer>(x => new RabbitMqConsumer(hostname, user, password, port));
 
             return services;
         }
